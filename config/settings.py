6f90f499ae2d869965e1e458e304cc33b42da80e
Damin <svs0371@naver.com>
2020-08-14 14:34:10 +0900

    조회수 기능

--- config/settings.py
+++ config/settings.py
@@ -196,3 +196,6 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 )
+
+DOMAIN_ADDRESS=''
+API_DOMAIN_ADDRESS='/'
---
+++ config/utils.py
@@ -0,0 +1,47 @@
+import datetime
+import json
+
+
+def hitCountRespose(request, response, object):
+
+    # [1] 로그인 확인
+    if not request.user.is_authenticated:
+        cookie_name = 'DDhit'
+    else:
+        cookie_name = f'DDhit:{request.session["authUser"]["id"]}'
+
+    # [2] 그 날 당일 밤 12시에 쿠키 삭제
+    tomorrow = datetime.datetime.replace(datetime.datetime.now(), hour=23, minute=59, second=0)
+    expires = datetime.datetime.strftime(tomorrow, "%a, %d-%b-%Y %H:%M:%S GMT")
+    objName = object.__class__.__name__
+    # [3] hit를 check하는 쿠키가 있는 경우
+    if request.COOKIES.get(cookie_name) is not None:
+        cookies = request.COOKIES.get(cookie_name)
+        cookies_dict = json.loads(cookies)
+        if objName in cookies_dict:
+            if not object.id in cookies_dict[objName]:
+                cookies_dict[objName]=cookies_dict[objName].append(object.id)
+                object.hitNums += 1
+                object.save()
+                returnCookies = json.dumps(cookies_dict)
+                response.set_cookie(cookie_name, returnCookies, expires=expires)
+
+        else:
+            cookies_dict[objName]=[object.id]
+            object.hitNums += 1
+            object.save()
+            returnCookies = json.dumps(cookies_dict)
+            response.set_cookie(cookie_name, returnCookies, expires=expires)
+        return response
+
+
+    # [4] hit를 check하는 쿠키가 없는 경우
+
+    cookies_dict={objName:object.id}
+    object.hitNums += 1
+    object.save()
+    returnCookies = json.dumps(cookies_dict)
+    response.set_cookie(cookie_name, returnCookies, expires=expires)
+
+    return response
+
